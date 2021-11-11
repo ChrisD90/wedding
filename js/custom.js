6e990ee8cb6ba14fec45eecbb01c89c0ebb57edc
Christoph Doeringer <christoph.doeringer@gmail.com>
2021-11-11 15:35:48 +0100

    changes

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@ $(function () {
     document.getElementById("countdown").style.display = "none";
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Jul 16, 2021 00:00:00").getTime();
+    var countDownDate = new Date("Jul 16, 2022 00:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
